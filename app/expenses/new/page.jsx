069461a884c001fc1f14dd97a8301420a40b425e
Rakshit Rabugotra <rakshit.rabugotra000@gmail.com>
2024-02-08 02:25:51 +0530

    Add bottom-margin to make scrollable views

--- app/expenses/new/page.jsx
+++ app/expenses/new/page.jsx
@@ -2,7 +2,7 @@ import CreateExpense from "./CreateExpense";
 import styles from './new-expense.module.css';
 
 export default function CreateNewExpense() {
-    return (<div className={styles.formContainer}>
+    return (<div className={styles.page}>
         <CreateExpense/>
     </div>);
 }
