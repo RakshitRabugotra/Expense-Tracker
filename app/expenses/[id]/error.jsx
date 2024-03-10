e5a1f597fa27d72ca9730170ba0bfd5552aa9460
Rakshit Rabugotra <rakshit.rabugotra000@gmail.com>
2024-03-10 18:18:22 +0530

    Remove unneccsary route

--- app/expenses/[id]/error.jsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-export default function ErrorExpense({ error, reset }) {
-
-    return (<div>
-        <p>An error occurred: {error.message}</p>
-        <button onClick={() => reset()}>Retry</button>
-    </div>);
-}
