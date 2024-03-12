3a98d88273a5749e3ec033c8ed0cb9ec5ad9c87e
Rakshit Rabugotra <rakshit.rabugotra000@gmail.com>
2024-03-12 06:45:20 +0530

    Add new progress style loader

--- app/(components)/ProgressLoader.jsx
+++ app/(components)/ProgressLoader.jsx
@@ -1,9 +1,18 @@
 import styles from "./loader.module.css";
 
-export default function Loader({ heading, context }) {
-  return (
+export default function Loader({ heading, context, isPage }) {
+  return isPage ? (
     <div className={styles.loadingPage}>
       {heading && <h1>{heading} {context && <span>{context}</span>}</h1>}
+      <div className={styles.progressLoader}></div>
+    </div>
+  ) : (
+    <div style={{
+      width: "100%",
+      display: "inline-flex",
+      justifyContent: "center",
+      padding: "8px 0px",
+      }}>
       <div className={styles.progressLoader}></div>
     </div>
   );
